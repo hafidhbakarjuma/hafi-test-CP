8ee2f28a279e1f450eac8e528708b9e834a74c9a
hafi Demba <hafidhbakar504@gmail.com>
2024-07-23 18:24:51 +0200

    do while loop update

---
+++ loop.c/do.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main ()
+{
+// This is the do loop it check everythis and than it going to check condition
+int a = 0;
+do {
+
+    printf("%d\n", a);
+    a++;
+} while(a < 10);
+
+
+    return 0;
+}
+
--- loop.c/for.c
+++ loop.c/for.c
@@ -7,9 +7,8 @@ int main ()
 // This is the for loop 
 for (int i = 0; i < 30; i++)
 {
-    printf("%d\n", i);
+    printf("%d\n", i) 
 }
-
 
     return 0;
 }
