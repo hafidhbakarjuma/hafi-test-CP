091d2f57a1cb19fad6c46292601f5a5883537f01
hafi Demba <hafidhbakar504@gmail.com>
2024-07-24 17:13:42 +0200

    while loop update

---
+++ loop.c/while.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main ()
+{
+ // This is the while loop on C first it check condition
+int a = 10;
+while(a > 0)
+{
+    printf("%d\n", a);
+    a--;
+}
+
+    return 0;
+}
