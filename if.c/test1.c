b201640fd6c47a5038a225715a7729b516644154
hafi Demba <hafidhbakar504@gmail.com>
2024-07-24 17:30:46 +0200

    If statement update

---
+++ if.c/test1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main ()
+{
+
+int a = 10;
+int b = a;
+
+// This is a IF statement on C
+if (a == b)
+{
+      printf("A is equal to B\n");   
+} else 
+{
+    printf("A is not equal to B");
+}
+
+    return 0;
+}
