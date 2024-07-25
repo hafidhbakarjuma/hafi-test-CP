f5e8a88944c7fc780a01e6598777fe8585827a9c
hafi Demba <hafidhbakar504@gmail.com>
2024-07-25 17:38:52 +0200

    intro test2 how to write your name on C

---
+++ intro.c/test2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+int main ()
+{
+
+//This is a Variable 
+int a;
+
+// This is a string 
+char b[20];
+
+printf("What is your name? ");
+fgets(b, 20, stdin);
+printf("How old are you? ");
+scanf("%d", &a);
+
+printf("Your name is %s\n", b);
+printf("Your age is %d\n", a);
+
+    return 0;
+}
