26e9a3ec6c49e739e3beab9bff5d1f34c88e46c0
hafi Demba <hafidhbakar504@gmail.com>
2024-07-26 11:24:15 +0200

    Function on C update

---
+++ function.c/test1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+// This is a Function on C
+void Birthday ()
+{
+
+printf("Happy Birthday to You Demba\n");
+printf("You Are 100 Years Old Now\n");
+printf("Welcome To The New World\n");
+
+}
+
+int main ()
+{
+
+Birthday();
+
+    return 0;
+}
