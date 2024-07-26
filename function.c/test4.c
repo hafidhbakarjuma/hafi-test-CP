d22c20265b30ac17bb860830991999f3ad74be67
hafi Demba <hafidhbakar504@gmail.com>
2024-07-26 12:28:44 +0200

    test4 function prototype

---
+++ function.c/test4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+// This is Function Prototype
+// You need to mention the argument before the main function
+// You need to mention the value inside the main function
+// It going to define the function after the main function
+
+void You(int name [], int age);
+
+int main ()
+{
+
+int age = 100;
+char name [] = "Hafi";
+
+You(name,age);
+
+    return 0;
+}
+    void You(int name[], int age)
+    {
+
+printf("Heloo %s\n", name);
+printf("You age is %d\n", age);
+
+    
+}
