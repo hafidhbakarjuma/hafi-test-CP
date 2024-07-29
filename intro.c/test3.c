a547d750eba7588d3db223808c7b2ce114924764
hafi Demba <hafidhbakar504@gmail.com>
2024-07-29 08:59:56 +0200

    intro test3 Variable

---
+++ intro.c/test3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main ()
+{
+// Variable
+
+int age = 20;
+char grade = 'c';
+float gpa = 21.1;
+double num = 23;
+
+printf("You age is %d\n", age);
+printf("Grade is %c\n", grade);
+printf("GPA is %f\n", gpa);
+printf("Your num is %lf\n", num);
+
+
+return 0;
+ }
