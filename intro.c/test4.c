1bbefa5c4e20eee17285b3afd73fe2f77b7e0aae
hafi Demba <hafidhbakar504@gmail.com>
2024-07-29 17:12:35 +0200

    intro test4 Multiplication

---
+++ intro.c/test4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main ()
+{
+
+// MULTIPLICATION 
+
+int a = 2+2+2+2+2+2+2;
+int b = 100-2-2-2-2-2-2;
+float c = 2*2*2*2*2*2;
+double d = 2/2/2/2/2;
+
+printf("A is %d\n", a);
+printf("B is %d\n", b); 
+printf("C is %f\n", c); // You can use %.2lf to make less cent 
+printf("D is %lf\n", d);
+    return 0;
+}
