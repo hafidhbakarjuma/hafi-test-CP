7429e48c7fd9c6a81b821db1fd4eb15f3761ea68
hafi Demba <hafidhbakar504@gmail.com>
2024-07-30 16:56:43 +0200

    for looo with alphabet

---
+++ loop.c/for1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main ()
+// FOR LOOP WITH ALPHABET
+{
+char ch;
+ for (ch = 'Z'; ch >= 'A'; ch--) // for(ch = 'A; ch<='Z'; ch++) for A TO Z
+ printf("%c\n", ch);
+ 
+return 0;
+}
