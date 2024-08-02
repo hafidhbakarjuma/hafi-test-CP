f1963e6e5fdc0b86a1cbf1057f536a207e1b4fd5
hafi Demba <hafidhbakar504@gmail.com>
2024-08-02 15:42:42 +0200

    bool 01 bool with IF

---
+++ bool.c/01.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main (void)
+{
+
+// Boolean with if statement
+
+int a = 0;
+int b = 10;
+if(a < b)
+{
+    printf("true ");
+} else
+{
+    {
+        printf("false ");
+    }
+}
+
+    return 0;
+}
