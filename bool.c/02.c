6664ffb956edf334a3c7d706324b6b9ece9d1e73
hafi Demba <hafidhbakar504@gmail.com>
2024-08-02 16:07:07 +0200

    bool 02 t use bool as viriable

---
+++ bool.c/02.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main ()
+{
+// We can use bool like variable
+// Also you need to mention <stdbool.h> in order to use bool
+bool a = 0;
+bool b = 1;
+    if(a > b) printf("true ");
+    else printf("false ");
+
+
+    return 0;
+}
